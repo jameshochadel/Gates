16113b0036d97fcb6d9ac57bb9d855f255c97303
James Hochadel <james.hochadel@gmail.com>
2014-01-09 23:28:17 -0500

    I don't know why these were excluded but they're not now

---
+++ Gates/GElements/GContainer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Gates.GElements
+{
+    public sealed class GContainer : GComponent
+    {
+        public GContainer()
+        {
+            this.DefaultStyleKey = typeof(GContainer);
+        }
+
+        /// <summary>
+        /// Change the input of the GElement that this GPrimitive outputs to.
+        /// Future note: Add "if" statement to prevent calling Propagate() unless
+        /// the value set is different from the value that was already in place.
+        /// </summary>
+        /// <param name="inputElement">The element sending the updated output</param>
+        /// <param name="value">The new value</param>
+        public override async void SetInput(GElement inputElement, bool value)
+        {
+
+        }
+
+        /// <summary>
+        /// Set the output value of this GPrimitive, and notify the GElement
+        /// connected to the output that its value has changed. 
+        /// I kept this separate from the property itself in case an output would
+        /// ever need to be changed without the results propagating. 
+        /// </summary>
+        /// <param name="newOutput">The new output value</param>
+        public override void SetOutput(bool newOutput)
+        {
+
+        }
+
+        public override void Propagate()
+        {
+
+        }
+
+        public event EventHandler<int> OutputChanged;
+
+        /// <summary>
+        /// Create an event in the case that one of this GElement's outputs is changed. 
+        /// </summary>
+        /// <param name="numOutput">The number of the output that has changed</param>
+        protected void OnOutputChanged(int numOutput)
+        {
+            EventHandler<int> handler = OutputChanged;
+            if (handler != null)
+            {
+                handler(this, numOutput);
+            }
+        }
+    }
+}
--- Gates/Properties/AssemblyInfo.cs
+++ Gates/Properties/AssemblyInfo.cs
@@ -3,25 +3,25 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute outputValues to modify the information
+// set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gates")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © James Hochadel 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four outputValues:
+// Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the outputValues or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
